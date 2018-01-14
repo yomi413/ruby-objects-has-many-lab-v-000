275256b54a215837e9336810f5e7fa31df998b96
Yomaira Escano <yjescano@gmail.com>
2018-01-14 16:38:31 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,8 +9,8 @@ class Artist
 
   def add_song(song)
     @songs << song
-    song.artist = self
-    @@song_count += 1
+    # song.artist = self
+    # @@song_count += 1
   end
 
   def add_song_by_name(name)
